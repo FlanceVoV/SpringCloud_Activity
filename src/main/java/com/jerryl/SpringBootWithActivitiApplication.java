746826d490324bef4fac3aa9046bd303ba3d8253
jianghuafei@konyo.com.cn <aQ2844y2>
2018-04-23 08:25:23 +0800

    test

--- src/main/java/com/jerryl/SpringBootWithActivitiApplication.java
+++ src/main/java/com/jerryl/SpringBootWithActivitiApplication.java
@@ -1,7 +1,5 @@
 package com.jerryl;
 
-import org.activiti.user_group.CustomGroupEntityManagerFactory;
-import org.activiti.user_group.CustomUserEntityManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,18 +12,5 @@ public class SpringBootWithActivitiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithActivitiApplication.class, args);
 	}
-
-
-
-	@Bean
-	public CustomGroupEntityManagerFactory getCustomGroupEntityManagerFactory(){
-		return new CustomGroupEntityManagerFactory();
-	}
-
-	@Bean
-	public CustomUserEntityManagerFactory getCustomUserEntityManagerFactory(){
-		return new CustomUserEntityManagerFactory();
-	}
-
 
 }
